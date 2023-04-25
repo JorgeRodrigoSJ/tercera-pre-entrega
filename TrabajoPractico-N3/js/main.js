786e11bc6d12c86e008c5ee8c5ea5a643253c8d3
JorgeRodrigoSJ <121474120+JorgeRodrigoSJ@users.noreply.github.com>
2023-04-25 01:28:16 -0300

    correcion

--- TrabajoPractico-N3/js/main.js
+++ TrabajoPractico-N3/js/main.js
@@ -1,16 +1,21 @@
+//declaro las constantes
 const sector = document.querySelector("#sector"),
   nombre = document.querySelector("#nombre"),
   marca = document.querySelector("#marca"),
   hsKm = document.querySelector("#hsKm"),//tiene el type="number"
   anio = document.querySelector("#anio"),//tiene el type="number"
-  //falta el de sector
   img = document.querySelector("#img"),
   search = document.querySelector("#search"),
   tbody = document.querySelector("#table-body"),
   formMaquinas = document.querySelector("#formMaquinas");
 const radios = document.querySelectorAll('input[type="radio"]');
 
-//Maquinas ya guardadas en inventario de maquinas
+//const del buscador del final
+const selectoritem = document.querySelector("#selectoritem");
+const editor = document.querySelector("#editor");
+const formEditor = document.querySelector("#formEditor");
+
+//Maquinas ya guardadas en inventario de maquinas - array de objetos
 const maquinaria = [{
     item: 1,
     nombre: "Autoelevador 1",
@@ -41,13 +46,13 @@ const maquinaria = [{
 // valor = Contenido del elemento
 
 
-//Seteo variable maquinas, si LS vacio entonces maquinas = inventario
+//Seteo variable maquinas, si LS vacio entonces maquinas = maquinaria
 //inicializo la variable maquinas y le asigno un valor
 let maquinas = JSON.parse(localStorage.getItem("maquinaria")) || maquinaria;
 
 
 //Constructor del objeto maquina ¡ojo que la primera esta en mayuscula!
-function Maquina(item, nombre, marca, hsKm, anio, sector, img) {
+function Maquina(nombre, marca, hsKm, anio, sector, img) {
   this.item = maquinaria.length + 1;
   this.nombre = nombre;
   this.marca = marca;
@@ -59,19 +64,16 @@ function Maquina(item, nombre, marca, hsKm, anio, sector, img) {
 }
 
 
-console.log(maquinaria);
-
 /* Declaración de Funciones */
 //Cargar al inventario
 function cargarMaquinaria(arr, maquina) {
   arr.push(maquina);
 }
 
-
-console.log(maquinaria);
-
+console.log(maquinas);
 
 //Funciones de LS
+//Con JSON.stringify podemos transformar un objeto JavaScript a un string en formato JSON. 
 function guardarLS(arr) {
   localStorage.setItem("maquinaria", JSON.stringify(arr));
 }
@@ -80,7 +82,7 @@ function guardarLS(arr) {
 function filtrar(arr, filtro, param) {
   return arr.filter((el) => {
     /*  if (param == "item") {
-      return el.item <= parseFloat(filtro);
+      return el.item == parseFloat(filtro);
     } else {
       return el[`${param}`].includes(filtro.toLowerCase());
     } */
@@ -90,13 +92,19 @@ function filtrar(arr, filtro, param) {
   });
 }
 
+//Funcion para editar las horas o kilometros actuales de la maquina
+//tengo que buscar la posicion del array restandole 1 al valor del item ingresado
+//cuando ya tenga seleccionado el array, tengo que editarle el valor nuevo de las horas o kilometros haciendo hsKm.value=nuevo valor (algo asi)
+
+
+
 //Manipular el DOM
 function crearHtml(arr) {
   tbody.innerHTML = "";
 
   let html = "";
   for (const elem of arr) {
-    const {item,nombre, marca,hsKm,anio,sector,img} = elem;
+    const {item,nombre,marca,hsKm,anio,sector,img} = elem;
        
     html = `<tr>
   <td>${item}</td>
@@ -106,7 +114,7 @@ function crearHtml(arr) {
   <td>${anio}</td>
   <td>${sector}</td>
   <td><img src="${img}"/></td>
-  <td><button class="btn red" id="${hsKm}">Borrar</button></td>
+  <td><button class="btn red" id="${item}">Borrar</button></td>
   </tr>`;
     tbody.innerHTML += html;
   }
@@ -115,12 +123,26 @@ function crearHtml(arr) {
   const arrayBotones = document.querySelectorAll("td .btn");
   arrayBotones.forEach((btn) => {
     btn.addEventListener("click", () => {
-      maquinas = maquinas.filter((el) => el.hsKm != btn.id);
+      maquinas = maquinas.filter((el) => el.item != btn.id);
       guardarLS(maquinas);
       crearHtml(maquinas);
     });
   });
 }
+
+//editor de hs y km
+const btnCargar = document.getElementById("btnCargar");
+function respuestaClick (e){
+  e.preventDefault();
+  posicionArray = 1-selectoritem.value;
+  console.log(maquinas[posicionArray]);
+  maquinas[posicionArray].hsKm=editor.value;
+  console.log("las nuevas horas o kilometros de la maquina seleccionada son Hs/Km "+ maquinas[posicionArray].hsKm);
+  //ahora tengo que pushearlas y listo
+  
+}
+
+formEditor.addEventListener("submit",respuestaClick)
 
 /* Fin de funciones */
 
@@ -133,7 +155,6 @@ crearHtml(maquinas);
 formMaquinas.addEventListener("submit", (e) => {
   e.preventDefault();
   const nuevoMaquina = new Maquina(
-    //item.value,
     nombre.value,
     marca.value,
     hsKm.value,
